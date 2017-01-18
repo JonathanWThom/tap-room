bbd3eaaf1516bea793e9b6c0ba1bef3525d9cd43
Jonathan Thom <jonathan.thom1990@gmail.com>
2017-01-17 18:01:56 -0800

    Initial commit

---
+++ app/app.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  template: `
+  <div class="container">
+    <h1>Angular Boilerplate</h1>
+  </div>
+  `
+})
+
+export class AppComponent {
+
+}
