25bf5becfb7348e44577815ecc36cf958894117a
Jonathan Thom and Maxwell Cady <jonathan+maxwell@>
2017-01-18 08:29:06 -0800

    Hard code kegs and print list to page

--- app/app.component.ts
+++ app/app.component.ts
@@ -4,11 +4,23 @@ import { Component } from '@angular/core';
   selector: 'app-root',
   template: `
   <div class="container">
-    <h1>Angular Boilerplate</h1>
+    <h1>Tap Room</h1>
+    <ul>
+      <li *ngFor="let currentKeg of kegs">{{currentKeg.name}}</li>
+    </ul>
   </div>
   `
 })
 
 export class AppComponent {
+  kegs: Keg[] = [
+    new Keg('Wanderale', 'Wander Brewing', 5, 6, 'Belgian Ale'),
+    new Keg('Kulshan Red Ale', 'Kulshan Brewing', 5, 5, 'Red Ale'),
+    new Keg('Budweiser', 'Budweiser', 3, 2.5, 'Yellow Water')
+  ]
+}
 
+export class Keg {
+  public pints: number = 124;
+  constructor(public name: string, public brand: string, public price: number, public alcoholContent: number, public style: string) { }
 }
