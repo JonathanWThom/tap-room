a2d3dac692b9011789445995120bee004ca36e89
Jonathan Thom and Maxwell Cady <jonathan@>
2017-01-18 08:41:47 -0800

    Create functionality to edit individual properties of kegs

--- app/app.component.ts
+++ app/app.component.ts
@@ -6,8 +6,26 @@ import { Component } from '@angular/core';
   <div class="container">
     <h1>Tap Room</h1>
     <ul>
-      <li *ngFor="let currentKeg of kegs">{{currentKeg.name}}</li>
+      <li *ngFor="let currentKeg of kegs">
+        {{currentKeg.name}}<br>
+        <button class="btn" (click)="editKeg(currentKeg)">Edit</button>
+      </li>
     </ul>
+    <hr>
+    <div class="edit" *ngIf="selectedKeg">
+      <h2>Edit Keg</h2>
+      <label>Name</label>
+      <input [(ngModel)]="selectedKeg.name">
+      <label>Brand</label>
+      <input [(ngModel)]="selectedKeg.brand">
+      <label>Price</label>
+      <input [(ngModel)]="selectedKeg.price">
+      <label>Alcohol Content</label>
+      <input [(ngModel)]="selectedKeg.alcoholContent">
+      <label>Style</label>
+      <input [(ngModel)]="selectedKeg.style">
+      <button class="btn" (click)="finishedEditing()">Done</button>
+    </div>
   </div>
   `
 })
@@ -17,7 +35,17 @@ export class AppComponent {
     new Keg('Wanderale', 'Wander Brewing', 5, 6, 'Belgian Ale'),
     new Keg('Kulshan Red Ale', 'Kulshan Brewing', 5, 5, 'Red Ale'),
     new Keg('Budweiser', 'Budweiser', 3, 2.5, 'Yellow Water')
-  ]
+  ];
+
+  selectedKeg = null;
+
+  editKeg(clickedKeg) {
+    this.selectedKeg = clickedKeg;
+  };
+
+  finishedEditing() {
+    this.selectedKeg = null;
+  }
 }
 
 export class Keg {
